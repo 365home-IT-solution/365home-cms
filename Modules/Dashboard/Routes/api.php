6bc53a2e59760df7a93c08a57c70f6da6c03319b
mamblenguyen <nmquang1997@gmail.com>
2026-07-23 13:31:54 +0700

    Api overview , dashboard page data

--- Modules/Dashboard/Routes/api.php
+++ Modules/Dashboard/Routes/api.php
@@ -1,1 +1,10 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+use Modules\Dashboard\Http\Controllers\DashboardController;
+
+Route::middleware(['auth:sanctum', 'admin.api'])
+    ->prefix('admin/dashboard')
+    ->group(function () {
+        Route::get('/overview', [DashboardController::class, 'overview']);
+    });
